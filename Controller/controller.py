f891cd918d2db1873c07ce8319cbc532b8ebe3e5
Benjamin Himley <benjaminhimley85@gmail.com>
2014-11-21 13:44:23 -0500

    initial functions defining

---
+++ Controller/controller.py
@@ -0,0 +1,32 @@
+import model
+import view
+
+class Game:
+    def __init__ (self):
+        # display initial view
+        pass
+
+    def create_account(self, name):
+        pass
+
+    def start_game(self):
+        #get rpm from model
+        #give rpm to view
+        #save start time
+        pass
+
+    def next_round(self):
+        pass
+        #save last round stats (time, )
+        #get next rpm from model
+
+
+    def check_high_scores(self):
+        pass
+
+    def check_personal_stats(self):
+        pass
+
+    def end_game(self):
+        pass
+        #close db
