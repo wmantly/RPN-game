74e6148015f9038b23c6b2f40cad0cb79ec23301
Benjamin Himley <benjaminhimley85@gmail.com>
2014-11-21 17:38:17 -0500

    coordinating model & controllers

--- model.py
+++ model.py
@@ -1,66 +1,66 @@
 import random
 class User:
-	def __init__(self,name_given):
-		self.name = name_given
-		# maybe score should be here
+    def __init__(self,name_given):
+        self.name = name_given
+        # maybe score should be here
 
 class DB:
-	def __init__(self,db_name):
-		self.db_name = db_name+".db"
-	
-	def create_user(self):
-		pass
-	def update_user_stats(self):
-		pass
+    def __init__(self,db_name):
+        self.db_name = db_name+".db"
+    
+    def create_user(self):
+        pass
+    def update_user_stats(self):
+        pass
 
 class RPN:
-	def __init__(self):
-		self.equation = ""
-		self.answer_equation = ""
-		self.user_answer = ""
+    def __init__(self):
+        self.equation = ""
+        self.answer_equation = ""
+        self.user_answer = ""
 
-	def chooseRandom(self,operatorLimit,numberLimit):
-		Operators = ["+","-","*","/"]
-		chosenOperator = Operators[random.randint(0,operatorLimit)]
-		chosenFirstNumber = str(random.randint(1,numberLimit))
-		chosenSecondNumber = str(random.randint(1,numberLimit))
-		return chosenOperator, chosenFirstNumber, chosenSecondNumber
+    def chooseRandom(self,operatorLimit,numberLimit):
+        Operators = ["+","-","*","/"]
+        chosenOperator = Operators[random.randint(0,operatorLimit)]
+        chosenFirstNumber = str(random.randint(1,numberLimit))
+        chosenSecondNumber = str(random.randint(1,numberLimit))
+        return chosenOperator, chosenFirstNumber, chosenSecondNumber
 
-	# broken
-	def answer_equation(self,equation):
-		x = 0
-		ops = ["+", "-", "*"] # "\", "mod"
-		stack = []
-		while x < len(equation):
-			if not equation[x] in ops:
-				stack.append(equation[x])
-			else:
-				y = stack.pop()
-				stringy = stack.pop() + equation[x] + y
-				stack.append(str(eval(stringy)))
-			x += 1
-		return (stack[0])
+    # broken
+    def answer_equation(self,equation):
+        x = 0
+        ops = ["+", "-", "*"] # "\", "mod"
+        stack = []
+        while x < len(equation):
+            if not equation[x] in ops:
+                stack.append(equation[x])
+            else:
+                y = stack.pop()
+                stringy = stack.pop() + equation[x] + y
+                stack.append(str(eval(stringy)))
+            x += 1
+        return (stack[0])
 
 
-	def generate_equation(self,operatorLimit,numberLimit,lengthLimit):
-		chosenLength = random.randint(1,lengthLimit)
-		TestString = []
-		length = 0
+    def generate_equation(self,operatorLimit,numberLimit,lengthLimit):
+        chosenLength = random.randint(1,lengthLimit)
+        TestString = []
+        length = 0
 
-		while length < chosenLength:
-			currentOperator, currentFirstNumber, currentSecondNumber = self.chooseRandom(operatorLimit,numberLimit)
-			if length == 0:
-				TestString.append(currentFirstNumber)
-				TestString.append(currentSecondNumber)
-				TestString.append(currentOperator)
-			else:
-				TestString.append(currentFirstNumber)
-				TestString.append(currentOperator)
-			length += 1
-		return TestString
+        while length < chosenLength:
+            currentOperator, currentFirstNumber, currentSecondNumber = self.chooseRandom(operatorLimit,numberLimit)
+            if length == 0:
+                TestString.append(currentFirstNumber)
+                TestString.append(currentSecondNumber)
+                TestString.append(currentOperator)
+            else:
+                TestString.append(currentFirstNumber)
+                TestString.append(currentOperator)
+            length += 1
+        return TestString
 
-	def compare_user_answer(self,user_answer):
-		pass
+    def compare_user_answer(self,user_answer):
+        pass
 
 # test 
 rpn = RPN()
