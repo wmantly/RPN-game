71feda3cf5609d1c51fd23f32100cd80875eda5e
brendan gilroy <bdgilroy@gmail.com>
2014-11-21 18:08:51 -0500

    repaired RPN, after deleting half of code along with hard tabs

--- model.py
+++ model.py
@@ -14,55 +14,55 @@ class DB:
 		pass
 
 class RPN:
-	def __init__(self):
-		self.equation = ""
-		self.answer_equation = ""
-		self.user_answer = ""
+    def __init__(self, operatorLimit, numberLimit, lengthLimit):
+        self.expression = self.generate_expression(operatorLimit, numberLimit, lengthLimit)
+        self.solution = self.generate_solution(self.expression)
 
-	def chooseRandom(self,operatorLimit,numberLimit):
-		Operators = ["+","-","*","/"]
-		chosenOperator = Operators[random.randint(0,operatorLimit)]
-		chosenFirstNumber = str(random.randint(1,numberLimit))
-		chosenSecondNumber = str(random.randint(1,numberLimit))
-		return chosenOperator, chosenFirstNumber, chosenSecondNumber
+    def chooseRandom(operatorLimit,numberLimit):
+        Operators = ["+","-","*"] # "/"
+        chosenOperator = Operators[random.randint(0,operatorLimit)]
+        chosenFirstNumber = str(random.randint(1,numberLimit))
+        chosenSecondNumber = str(random.randint(1,numberLimit))
+        return chosenOperator, chosenFirstNumber, chosenSecondNumber
 
-	# broken
-	def answer_equation(self,equation):
-		x = 0
-		ops = ["+", "-", "*"] # "\", "mod"
-		stack = []
-		while x < len(equation):
-			if not equation[x] in ops:
-				stack.append(equation[x])
-			else:
-				y = stack.pop()
-				stringy = stack.pop() + equation[x] + y
-				stack.append(str(eval(stringy)))
-			x += 1
-		return (stack[0])
+    # broken
+    def generate_solution(self, expression):
+        x = 0
+        ops = ["+", "-", "*"] # "\", "mod"
+        stack = []
+        while x < len(expression):
+            if not expression[x] in ops:
+                stack.append(expression[x])
+            else:
+                y = stack.pop()
+                stringy = stack.pop() + expression[x] + y
+                stack.append(str(eval(stringy)))
+            x += 1
+        return (stack[0])
 
 
-	def generate_equation(self,operatorLimit,numberLimit,lengthLimit):
-		chosenLength = random.randint(1,lengthLimit)
-		TestString = []
-		length = 0
+    def generate_expression(self, operatorLimit, numberLimit, lengthLimit):
+        chosenLength = random.randint(1,lengthLimit)
+        TestString = []
+        length = 0
+        difficulty = 0 
 
-		while length < chosenLength:
-			currentOperator, currentFirstNumber, currentSecondNumber = self.chooseRandom(operatorLimit,numberLimit)
-			if length == 0:
-				TestString.append(currentFirstNumber)
-				TestString.append(currentSecondNumber)
-				TestString.append(currentOperator)
-			else:
-				TestString.append(currentFirstNumber)
-				TestString.append(currentOperator)
-			length += 1
-		return TestString
+        while length < chosenLength:
+            currentOperator, currentFirstNumber, currentSecondNumber = RPN.chooseRandom(operatorLimit, numberLimit)
+            if length == 0:
+                TestString.append(currentFirstNumber)
+                TestString.append(currentSecondNumber)
+                TestString.append(currentOperator)
+            else:
+                TestString.append(currentFirstNumber)
+                TestString.append(currentOperator)
+            length += 1
+        
+        return TestString
 
-	def compare_user_answer(self,user_answer):
-		pass
+    def compare_user_answer(self, user_answer):
+        pass
 
 # test 
-rpn = RPN()
-eq = rpn.genater_equation(2,10,5)
-print(eq)
+rpn = RPN(2,10, 7)
+print(rpn.expression)
