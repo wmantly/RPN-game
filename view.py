a54d85e88d82fe042472be4c6f6ca431d9c29fc1
william <wmantly@gmail.com>
2014-11-23 12:23:09 -0500

    fixed exit issue. should be done messing with contrller.py

--- view.py
+++ view.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python3
 
-import curses
 import time
-
+# import curses
 
 class View:
-    def __init__( self ):
+    def __init__( self, screen, curses ):
 
-        self.showDev = True
+        self.showDev = False
 
+        self.curses = curses
         # start the curses instance
-        self.screen = curses.initscr()
+        self.screen = screen
 
         # configure the whole screen
 
         # set border for main screen
         self.screen.border(0)
         # hide KB input
-        curses.noecho()
+        self.curses.noecho()
 
-        curses.curs_set(0)
+        self.curses.curs_set(0)
 
         # idk, i have to look it up
         self.screen.keypad(1)
@@ -28,22 +28,25 @@ class View:
         self.screen.refresh()
 
         # setup the header window
-        self.header = curses.newwin( 4, 35, 2, 3 )
+        self.header = self.curses.newwin( 4, 35, 2, 3 )
         self.header.border(0)
 
         # setup the right header window
-        self.header_right = curses.newwin( 4, 35, 2, 38 )
+        self.header_right = self.curses.newwin( 4, 35, 2, 38 )
         self.header_right.border(0)
         self.header_right.refresh()
 
         # setup the body window
-        self.body = curses.newwin( 11, 50, 6 , 3)
+        self.body = self.curses.newwin( 11, 50, 6 , 3)
         # self.body.border(0)
 
         # side bar
-        self.side_bar = curses.newwin( 11, 20, 6 , 53)
+        self.side_bar = self.curses.newwin( 11, 20, 6 , 53)
         self.side_bar.border(0)
 
+        # dev console
+        self.dev = self.curses.newwin( 50, 70, 17 , 3)
+        self.dev.border(0)
 
         #############################################
 
@@ -75,7 +78,7 @@ class View:
             event = self.screen.getch()
             if event == ord( "q" ) or event == ord( "Q" ):
                 # needs work...
-                curses.endwin()
+                self.curses.endwin()
                 exit()
             if event == ord( "n" ) or event == ord( "N" ):
                 return False
@@ -92,7 +95,7 @@ class View:
         self.body.clear()
 
         # allow user to see KB input
-        curses.echo()
+        self.curses.echo()
 
         # write to body
         self.body.addstr(2, 2, "Pick a new user name and pin:" )
@@ -109,7 +112,7 @@ class View:
 
         # if error message
         if message:
-            self.body.addstr( 7,2, message )
+            self.body.addstr( 7, 2, message )
             self.body.refresh()
 
         return( { 'name':name, 'password':password } )
@@ -123,10 +126,16 @@ class View:
         return sign_up( True )
 
     def login( self, message=False ):
+        self.devConsole( [ str( message ) ] )
         # remove old body content
         self.body.clear()
         # allow user to see KB input
-        curses.echo()
+        self.curses.echo()
+
+        if message:
+            self.curses.flash()
+            self.body.addstr( 7, 2, message )
+            self.body.refresh()
 
         self.body.addstr(2, 2, "Please log in with your user name and pin:" )
 
@@ -137,9 +146,7 @@ class View:
         self.body.refresh()
         password = self.body.getstr(6, 2, 60)
 
-        if message:
-            self.body.addstr( 7,2, message )
-            self.body.refresh()
+        # if error message
 
         return( { 'name':name, 'password':password } )
 
@@ -189,11 +196,11 @@ class View:
         answer = self.body.getstr(6, 3, 60)
         return answer
 
-    def dev(self, message ):
+    def devConsole( self, message ):
         # side bar
         if not self.showDev: return False
 
-        self.dev = curses.newwin( 50, 70, 17 , 3)
+        self.dev.clear()
         self.dev.border(0)
         
         count = 0
@@ -201,5 +208,5 @@ class View:
             count += 1
             self.dev.addstr( count, 2, i )
 
-            self.dev.refresh()
+        return self.dev.refresh()
 ##testing
